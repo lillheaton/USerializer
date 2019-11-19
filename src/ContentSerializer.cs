5a66059e207f8ba3a1ec17a842e5a9da9b6f11bf
Emil Olsson <emilolsson93@gmail.com>
2019-11-19 16:51:58 +0100

    Convert method checks if type has a converter

--- src/ContentSerializer.cs
+++ src/ContentSerializer.cs
@@ -10,10 +10,14 @@ namespace USerializer
 {
     public class ContentSerializer : IContentSerializer
     {
+        private readonly IConverterLocatorService _converterLocatorService;
+
         public JsonSerializerSettings JsonSerializerSettings { get; private set; }
-        
+
         public ContentSerializer(IConverterLocatorService converterLocatorService)
         {
+            _converterLocatorService = converterLocatorService;
+
             JsonSerializerSettings = new JsonSerializerSettings
             {
                 ContractResolver = new JsonContractSimplifier.ContractResolver(converterLocatorService, null)
@@ -33,12 +37,31 @@ namespace USerializer
         {
             Type targetType = target.GetType();
 
-            var properties = 
-                targetType
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(x => x.HasAttribute<JsonIgnoreAttribute>() == false);
+            // Could not find any converter
+            if(!_converterLocatorService.TryFindConverterFor(targetType, out IConverter converter))
+            {
+                var properties = targetType
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(x => x.HasAttribute<JsonIgnoreAttribute>() == false);
 
-            return ObjectUtils.ConvertProperties(target, properties);
+                return ObjectUtils.ConvertProperties(target, properties);
+            }
+
+            var convertedTarget = _converterLocatorService.Convert(target, converter);
+
+            // Converted target is already dictionary
+            if(convertedTarget is IDictionary<string, object>)
+            {
+                return new Dictionary<string, object>(convertedTarget as IDictionary<string, object>);
+            }
+            
+            var convertedTargetType = convertedTarget.GetType();
+
+            var convertedTargetProperties = targetType
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(x => x.HasAttribute<JsonIgnoreAttribute>() == false);
+
+            return ObjectUtils.ConvertProperties(target, convertedTargetProperties);
         }
 
     }
